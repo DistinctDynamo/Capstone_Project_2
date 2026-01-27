f8d6464e4dd2ade04f166fc58abf302db7bc3b15
DistinctDynamo <nguyensteven578@gmail.com>
2026-01-27 01:07:54 -0500

    setting up the server

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const DB_URL = "";
+const PORT = 8046;
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', (req, res) => {
+    res.send("<h1>Capstone 2 Soccer Connect App</h1>");
+});
+
+mongoose.connect(DB_URL).then(() => {
+    console.log("Successfully connected to the database mongoDB Atlas Server");    
+    app.listen(PORT, () => {
+    console.log("Server is listening on port 8046");
+});
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
